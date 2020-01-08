9b1948e2fc40d39abc5d46e135b7786532adc7b5
bocembocem <nzolotykh94@mail.ru>
2020-01-08 22:03:27 +0300

    add section contact

--- src/js/main.js
+++ src/js/main.js
@@ -1,4 +1,10 @@
 $(function() {
-        
+    let bgCount = 1;
+    $('.contact-block-top').each(function() {
+        $(this).css({
+            backgroundImage: 'url(../img/contact_banner' + bgCount +'.png)'
+        });
+        bgCount++;
+    });
 });
 
