91a86b9eb36ac31cb642447ccaebacdb04d19c9a
bocembocem <nzolotykh94@mail.ru>
2020-01-09 23:40:41 +0300

    add js for contact bg, read comment

--- src/js/main.js
+++ src/js/main.js
@@ -30,7 +30,9 @@ $(function() {
     /* section contact, blocks background rules */
 
     $('.contact-block-top').each(function() {
-        // npm run build 'url(./img/contact_banner' + contactBgCount +'.png)' or npm run dev // npm run build 'url(../img/contact_banner' + contactBgCount +'.png)'
+        // npm run build 'url(./img/contact_banner' + contactBgCount +'.png)' 
+        // or  
+        // npm run dev 'url(../img/contact_banner' + contactBgCount +'.png)'
         $(this).css({
             backgroundImage: 'url(./img/contact_banner' + contactBgCount +'.png)'
         });
